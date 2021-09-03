8817a29ca60927020bb61783625bac17fd445190
Vladislav <vzv@siluetstudio.com>
2021-09-03 15:52:59 +0300

    correct

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             pauseOnHover: false,
             pauseOnFocus: false,            
             customPaging : function(slider, i) {
-                return '<svg class="dots-svg" width="42" height="42" viewport="0 0 8 8" version="1.1" xmlns="http://www.w3.org/2000/svg"><circle id="bar" r="20" cx="21" cy="21" fill="none" stroke-dasharray="140" stroke-dashoffset="140" stroke="white" stroke-width="2"></circle></svg><span>'+ (i + 1) +'</span>';
+                return '<svg class="dots-svg" width="42" height="42" viewport="0 0 8 8" version="1.1" xmlns="http://www.w3.org/2000/svg"><circle id="bar" r="20" cx="21" cy="21" fill="none" stroke-dasharray="140" stroke-dashoffset="140" stroke="white" stroke-width="2"></circle></svg><span></span>';
             },
             autoplay: true,
             autoplaySpeed: 6000,
